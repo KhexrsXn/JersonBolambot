639e69de484ef46b47ea0aeb25706720f9cb0f31
KhexrsXn <serjonbolambot@gmail.com>
2023-10-11 17:33:32 +0800

    commit

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,51 @@
+export const website = [
+    {
+        img: './images/website.png',
+        title: 'DevKids Academy',
+        tools: ['HTML', 'CSS', 'JavaScript', 'Boostrap'],
+        description: ' "Devkids Academy" is an innovative e-learning platform designed specifically for children. This website has a diverse range of game categories, provides a fun and engaging environment where children can explore, learn, and grow. Its a front-end development group project during our KodeGo Bootcamp',    
+    },
+    {
+        img: './images/website.png',
+        title: 'Soria',
+        tools: ["JavaScript","React", "Bootstrap", "Nodejs", "PHP", ],
+        description: ' Soria is your go-to platform for easy hotel bookings. With Soria, you can choose from a variety of hotel rooms at affordable prices, see what they offer, and reserve your room with simplicity and convenience. Its a full stack development group project during our KodeGo Bootcamp',    
+    },
+    {
+        img: './images/website.png',
+        title: 'ERP',
+        tools: ["JavaScript","React", "Bootstrap", "MUI", "Python", "Django",  ],
+        description: ' Soria is your go-to platform for easy hotel bookings. With Soria, you can choose from a variety of hotel rooms at affordable prices, see what they offer, and reserve your room with simplicity and convenience. Its a full stack development group project during our KodeGo Bootcamp',  
+    }    
+]
+
+export const ux_ui = [
+    {
+        img: './images/website.png',
+        title: 'DevKids Academy',
+        description: ' "Devkids Academy" is an innovative e-learning platform designed specifically for children. This website has a diverse range of game categories, provides a fun and engaging environment where children can explore, learn, and grow. Its a front-end development group project during our KodeGo Bootcamp',    
+    },
+    {
+        img: './images/website.png',
+        title: 'Soria',
+        description: ' Soria is your go-to platform for easy hotel bookings. With Soria, you can choose from a variety of hotel rooms at affordable prices, see what they offer, and reserve your room with simplicity and convenience. Its a full stack development group project during our KodeGo Bootcamp',    
+    },
+    {
+        img: './images/website.png',
+        title: 'ERP',
+        description: ' Soria is your go-to platform for easy hotel bookings. With Soria, you can choose from a variety of hotel rooms at affordable prices, see what they offer, and reserve your room with simplicity and convenience. Its a full stack development group project during our KodeGo Bootcamp',
+    }
+]
+
+export const certifications = [
+    {
+        img: './images/certification.png',
+        title: 'Full Stack Developer',
+        description: `I'm a Full Stack Developer with a solid foundation from KodeGo's intensive bootcamp, specializing in web development. I excel in problem-solving and bring a wealth of hands-on experience to drive innovation and success in the tech industry.`,
+    },
+    {
+        img: './images/certification.png',
+        title: 'Responsive Web Design',
+        description: 'This credential from FreeCodeCamp signifies expertise in creating user-friendly websites that adapt to various devices.',
+    }
+]
